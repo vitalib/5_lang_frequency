67b43dae53095d4b5e4779f35403e95a964ff1a2
Vitali Baranov <vitali.baranov@gmail.com>
2017-10-27 12:21:41 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# Frequency Analysis of Words
+# Frequency Analysis of Words 
+Script prints 10 most frequent word of a given file.
 
-[TODO. There will be project description]
+# Quickstart
+Requires Python3.5. Name of analyzed file should be provided as an argument
+of command line.
+
+Example of utilization for Linux
+```bash
+python3 lang_frequency.py <filename> 
+Most frequent words:
+text      12
+file      10
+word      10
+frequency 9
+words     8
+def       6
+get       6
+encoding  6
+dict      6
+filepath  5
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,58 @@
+import string
+import argparse
+import chardet
+
+
+def get_encoding(filepath):
+    with open(filepath, 'rb') as a_file:
+        return chardet.detect(a_file.read())['encoding']
 
 
 def load_data(filepath):
-    pass
+    file_encoding = get_encoding(filepath)
+    with open(filepath, encoding=file_encoding) as a_file:
+        return a_file.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    frequences_dict = dict()
+    for word in text.split():
+        if word in frequences_dict:
+            frequences_dict[word] += 1
+        else:
+            frequences_dict[word] = 1
+    return sorted(frequences_dict.items(),
+                  key=lambda word_freq: word_freq[-1],
+                  reverse=True,
+                  )[:10]
+
+
+def get_file_name():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    return parser.parse_args().filename
+
+
+def delete_punctuation(text):
+    for sign in string.punctuation:
+        text = text.replace(sign, ' ')
+    return text
+
+
+def print_frequency(words_frequency):
+    longest_word = max(words_frequency,
+                       key=lambda frequency: len(frequency[0])
+                       )[0]
+    max_field_width = len(longest_word)
+    print('Most frequent words:')
+    for word_freq in words_frequency:
+        print('{:{width}} {}'.format(*word_freq, width=max_field_width))
 
 
 if __name__ == '__main__':
-    pass
+    file_name = get_file_name()
+    text = load_data(file_name)
+    text = text.lower()
+    text = delete_punctuation(text)
+    words_frequency = get_most_frequent_words(text)
+    print_frequency(words_frequency)
