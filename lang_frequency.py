884c3641b3584905391e33407358b3c66c758968
Vitali Baranov <vitali.baranov@gmail.com>
2017-10-28 12:54:55 +0300

    style modifications

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,9 +4,6 @@ import argparse
 import chardet
 
 
-TOP_QUANTITY = 10
-
-
 def get_encoding(filepath):
     with open(filepath, 'rb') as a_file:
         return chardet.detect(a_file.read())['encoding']
@@ -14,14 +11,14 @@ def get_encoding(filepath):
 
 def load_data(filepath):
     file_encoding = get_encoding(filepath)
-    with open(filepath, encoding=file_encoding) as a_file:
-        return a_file.read()
+    with open(filepath, encoding=file_encoding) as text_file:
+        return text_file.read()
 
 
-def get_file_name():
+def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
-    return parser.parse_args().filename
+    return parser.parse_args()
 
 
 def print_frequency(words_frequency):
@@ -30,15 +27,19 @@ def print_frequency(words_frequency):
                        )[0]
     max_field_width = len(longest_word)
     print('Most frequent words:')
-    for word_freq in words_frequency:
-        print('{:{width}} {}'.format(*word_freq, width=max_field_width))
+    for word, freq in words_frequency:
+        print('{:{width}} {}'.format(word, 
+                                     freq,
+                                     width=max_field_width)
+                                     )
 
 
 if __name__ == '__main__':
-    file_name = get_file_name()
+    args = get_arguments()
+    file_name = args.filename
     text = load_data(file_name)
     all_words = re.findall(r'\w+', text.lower())
     most_frequent_words = (
-        collections.Counter(all_words).most_common(TOP_QUANTITY)
+        collections.Counter(all_words).most_common(n=10)
         )
     print_frequency(most_frequent_words)
